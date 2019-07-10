6b6a6b31a45d6f6c3930a1bfedb170e78b1fad1f
doroddong <dongpang1014@ajou.ac.kr>
2019-07-11 02:59:13 +0900

    "Make CustomCalculatorTest Using assertThat by LeeDongHun"

--- src/test/java/CustomCalculatorTest.java
+++ src/test/java/CustomCalculatorTest.java
@@ -14,7 +14,9 @@ public class CustomCalculatorTest {
     public void add() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.add(10,15);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertThat(result,is(25));          //by LeeDongHun
+
         System.out.println("result :: " + result);
     }
 
@@ -23,7 +25,9 @@ public class CustomCalculatorTest {
     public void subtract() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.subtract(23,10);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertThat(result,is(13));          //by LeeDongHun
+
         System.out.println("result :: " + result);
     }
 
@@ -32,7 +36,9 @@ public class CustomCalculatorTest {
     public void multiply() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.multiply(5,9);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertThat(result,is(45));          //by LeeDongHun
+
         System.out.println("result :: " + result);
     }
 
@@ -41,7 +47,9 @@ public class CustomCalculatorTest {
     public void divide() {
         customCalculator = new CustomCalculator();
         int result = customCalculator.divide(25,5);
-        /* 이곳에 테스트 코드를 작성하세요. */
+
+        assertThat(result,is(5));           //by LeeDongHun
+
         System.out.println("result :: " + result);
     }
 }
