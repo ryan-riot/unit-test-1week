9c60e973de02c1886fbf40bf0a2730c79bd80adb
doroddong <dongpang1014@ajou.ac.kr>
2019-07-11 02:51:52 +0900

    "Make UpperCaseCounterTest by LeeDongHun"

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -15,15 +15,18 @@ public class UpperCaseCounterTest {
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertThat(numberOfUpperCaseCharactersInString,is(0));          //by LeeDongHun
     }
 
-    //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
+    //빈값을 전달했을 때 0을 리턴하는지에 대한 테스트 작성
     @Test
     public void getNumberOfUpperCaseCharactersInString_return_0_for_empty_input() {
         String str = "";
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+
+        assertThat(numberOfUpperCaseCharactersInString,is(0));          //by LeeDongHun
     }
 
     //대문자들이 포함된 문자열을 전달했을 때 카운팅된 숫자와 맞는지 검증하는 테스트 작성
@@ -33,9 +36,7 @@ public class UpperCaseCounterTest {
 
         int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
 
-        //assertTrue로 맞는 테스트 코드 작성
-        //assertFalse로 틀리는 값을 넣어 테스트 작성
-        //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+        assertThat(numberOfUpperCaseCharactersInString,is(10));         //by LeeDongHun
     }
 
     //대소문자가 섞여 있을 때 정확히 카운팅 되는 지에 대한 테스트 코드 작성
@@ -43,21 +44,22 @@ public class UpperCaseCounterTest {
     public void getNumberOfUpperCaseCharacterInString_return_6_for_ABCdefGHI(){
         String str = "ABCdefGHI";
         int result = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
-        //assertTrue로 맞는 테스트 코드 작성
-        //assertFalse로 틀리는 값을 넣어 테스트 작성
-        //assertThat 단정문을 사용해서 True인 테스트 코드 작성
+
+        assertThat(result,is(6));                                       //by LeeDongHun
+
         System.out.println("result :: " + result);
     }
 
     //잘못된 값을 참조했을 때 IndexOutOfBoundsException Exception이 발생하는지 테스트 코드 작성
-    @Test
+    @Test(expected = IndexOutOfBoundsException.class)
     public void shouldThrowExceptionWhenGetZeroIndex() {
         new ArrayList<Object>().get(0);
     }
 
     //해당 메소드가 제한된 시간내에 실행되는지에 대한 테스트 코드 작성 : timeout 사용
     //두번째로 해당 메소드는 테스트 하지 않도록 어노테이션 추가 적용 해봅니다. Ignore
-    @Test
+
+    @Test(timeout = 5000)
     public void testShouldRunInLimitedTime() throws InterruptedException {
         Thread.sleep(4000);
         System.out.println("제한된 시간 내에 수행되면 테스트 Passed!");
