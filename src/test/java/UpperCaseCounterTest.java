1b8363c36261d0fb194a407611b65a0bc6a2e48f
Ryan Cheong <lastboy0@gmail.com>
2021-04-22 14:34:26 +0900

    Remove test code

--- src/test/java/UpperCaseCounterTest.java
+++ src/test/java/UpperCaseCounterTest.java
@@ -15,7 +15,8 @@ public class UpperCaseCounterTest {
     public void getNumberOfUpperCaseCharactersInString_return_0_for_null_input(){
         String str = null;
 
-//        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        int numberOfUpperCaseCharactersInString = upperCaseCounter.getNumberOfUpperCaseCharactersInString(str);
+        assertTrue(numberOfUpperCaseCharactersInString == 0);
     }
 
     //반겂울 전달했을 때 0을 리턴하는지에 대한 테스트 작성
